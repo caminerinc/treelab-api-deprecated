1c9d6ecca9fa9f454bdfbc2b257853d2095a1038
Lim Wang <lim@caminer.io>
2018-12-20 14:16:46 +0800

    优化

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -4,7 +4,6 @@ const path = require('path');
 const CryptoJS = require('crypto-js');
 
 module.exports = {
-  randomString,
   md5,
   sha1,
 };
@@ -15,15 +14,4 @@ function md5(text) {
 
 function sha1(text) {
   return CryptoJS.SHA1(text.toString()).toString();
-}
-
-function randomString(len) {
-  len = len || 32;
-  let chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';  //默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1
-　let maxPos = chars.length;
-　let result = '';
-　for (let i = 0; i < len; i++) {
-    result += chars.charAt(Math.floor(Math.random() * maxPos));
-　}
-　return result;
 }
--- app/router.js
+++ app/router.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = app => {
-
   //api
   app.router.get('/api/health-check', app.controller.api.healthCheck);
 };
