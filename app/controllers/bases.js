45e2a65c94fc794ccf3011ebf88707ed1e96a3ca
limwcj <45704878+limwcj@users.noreply.github.com>
2018-12-24 13:43:03 +0800

    createUid方法修改，获取全部base接口/api/bases，创建接口/api/base (#7)

--- app/controllers/bases.js
+++ app/controllers/bases.js
@@ -49,4 +49,16 @@ module.exports = {
       ],
     });
   },
+
+  findBases() {
+    return Bases.findAll({
+      attributes: ['id', 'name'],
+    });
+  },
+
+  createBase(params) {
+    return Bases.create({
+      name: params.name,
+    });
+  },
 };
