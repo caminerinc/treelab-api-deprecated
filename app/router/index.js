e0305b94ce6334691f6d5acb6a383f1e87dd26d9
CaiMinHao <923145852@qq.com>
2018-12-24 14:24:56 +0800

    Get users / 获取所有的用户
    1. 建立新的users model，migration，seed
    2. user 里面有 id, firstName, lastName, email, passwordDigest,
    3. id 一定要用 createUid(): https://github.com/caminerinc/elephante-api/blob/develop/app/models/fields.js#L11
    你可以用prefix: 'usr'
    4. 让后你开一个新的API叫/api/users，就是那所有的用户。
    5. 如果我们可以用链接拿到所有的用户，就成功了

--- app/router/index.js
+++ app/router/index.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 const router = new Router();
 
 const basesResolver = require('../resolvers').bases;
+const usersResolver = require('../resolvers').users;
 
 // App
 router.get('/api/health-check', ctx => {
@@ -11,5 +12,8 @@ router.get('/api/health-check', ctx => {
 // Base
 // @[DEREK] Delete this after real ones have been implemented
 router.get('/api/base/:baseId', basesResolver.getBaseById);
+
+//Users
+router.get('/api/users', usersResolver.users);
 
 module.exports = router;
