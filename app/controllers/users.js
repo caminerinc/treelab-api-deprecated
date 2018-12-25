9181a99cf7ca672ec85d666947d0e4975ca4ccaa
CaiMinHao <923145852@qq.com>
2018-12-25 09:49:39 +0800

    Revert "Merge branch 'develop' of https://github.com/caminerinc/elephante-api into develop"
    This reverts commit c0b14a4902fa275ccd003c9d1a2e091a5ab76152, reversing
    changes made to 046f22362fe6c1d41bb3081051ca9829e718f088.

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,19 +1,10 @@
 const Users = require('../models').users;
-const helper = require('../util/helper');
 
 module.exports = {
   findAllUser() {
     return Users.findAll({
       attributes: ['id', 'firstName', 'lastName', 'email', 'passwordDigest'],
       raw: true,
-    });
-  },
-  createUser({ firstName, lastName, password, email }) {
-    return Users.create({
-      firstName,
-      lastName,
-      passwordDigest: helper.sha1(password),
-      email,
     });
   },
 };
--- app/router/index.js
+++ app/router/index.js
@@ -16,6 +16,5 @@ router.post('/api/base', basesResolver.createBase);
 
 //Users
 router.get('/api/users', usersResolver.users);
-router.post('/api/user', usersResolver.user);
 
 module.exports = router;
