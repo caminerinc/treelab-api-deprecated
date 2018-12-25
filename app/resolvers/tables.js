2e9d1edb9158cfc2145a2bf29a9e086476446cfb
limwcj <45704878+limwcj@users.noreply.github.com>
2018-12-25 09:39:19 +0800

    接口/api/getTables 和一些优化 (#10)
    * tables接口
    
    * db_config for example
    
    * tables接口
    
    * 包优化
    
    * 优化

---
+++ app/resolvers/tables.js
@@ -0,0 +1,27 @@
+const { pick } = require('lodash');
+
+const tablesController = require('../controllers').tables;
+
+const adaptTables = tables => {
+  return {
+    tableSchemas: tables.map(table => ({
+      ...pick(table, ['id', 'name']),
+      columns: table.fields.map(field => ({
+        ...pick(field, ['id', 'name']),
+        type: field.type.name,
+      })),
+    })),
+  };
+};
+
+module.exports = {
+  async getTables(ctx) {
+    let params = ctx.params;
+    if (!params.baseId) {
+      ctx.status = 422;
+      return (ctx.body = { message: 'baseId is required' });
+    }
+    let tables = await tablesController.findTables(params.baseId);
+    ctx.body = adaptTables(tables);
+  },
+};
