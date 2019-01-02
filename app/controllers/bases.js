ccbf64db2a90b1a18fde179688b2e496586274df
limwcj <45704878+limwcj@users.noreply.github.com>
2019-01-02 11:54:56 +0800

    /api/bases 加 primaryTableId (#25)

--- app/controllers/bases.js
+++ app/controllers/bases.js
@@ -1,9 +1,16 @@
-const { bases } = require('../models');
+const { bases, tables } = require('../models');
 
 module.exports = {
   dbGetBases() {
     return bases.findAll({
       attributes: ['id', 'name'],
+      include: [
+        {
+          model: tables,
+          as: 'tables',
+          attributes: [['id', 'primaryTableId']],
+        },
+      ],
     });
   },
 
