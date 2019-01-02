b890193a5c5e02aba16a4380192b450bee9b1633
Lim Wang <lim@caminer.io>
2019-01-02 11:43:30 +0800

    /api/bases 加 primaryTableId

--- app/resolvers/bases.js
+++ app/resolvers/bases.js
@@ -2,10 +2,21 @@ const { pick } = require('lodash');
 const { checkKeyExists } = require('../util/helper');
 const { dbGetBases, dbCreateBase } = require('../controllers/bases');
 
+const adaptBases = bases => {
+  return Array.from(bases, base => {
+    base = JSON.parse(JSON.stringify(base));
+    return {
+      id: base.id,
+      name: base.name,
+      primaryTableId: base.tables[0] ? base.tables[0].primaryTableId : null,
+    };
+  });
+};
+
 module.exports = {
   async getBases(ctx) {
     const bases = await dbGetBases();
-    ctx.body = { bases: bases };
+    ctx.body = { bases: adaptBases(bases) };
   },
 
   async createBase(ctx) {
