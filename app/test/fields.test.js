b650a8d1d24bbe1f3f85281ec7ff76cb9c549250
Caminer-Carson <45706114+Caminer-Carson@users.noreply.github.com>
2019-01-10 09:06:14 +0800

    Test post field post record (#42)

---
+++ app/test/fields.test.js
@@ -0,0 +1,195 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const expect = chai.expect;
+const { findIndex } = require('lodash');
+
+function checkNewField(params, done) {
+  chai
+    .request('http://localhost:8000')
+    .get('/api/tables/bse1jT7ZIHLmjH4')
+    .end((err, res) => {
+      res.should.have.status(200);
+
+      let columns = res.body.tableSchemas[0].columns;
+      const field = findIndex(columns, function(o) {
+        return o.id == params.fieldId;
+      });
+      field.should.be.not.eql(-1);
+      done();
+    });
+}
+function checkForeignField(params, done) {
+  chai
+    .request('http://localhost:8000')
+    .get('/api/tables/bse1jT7ZIHLmjH4')
+    .end((err, res) => {
+      res.should.have.status(200);
+
+      let foreignColumns = res.body.tableSchemas[0].columns;
+      let symmetricColumns = res.body.tableSchemas[1].columns;
+      const foreignField = findIndex(foreignColumns, function(o) {
+        return o.id == params.foreignFieldId;
+      });
+      const symmetricField = findIndex(symmetricColumns, function(o) {
+        return o.id == params.symmetricFieldId;
+      });
+      foreignField.should.be.not.eql(-1);
+      symmetricField.should.be.not.eql(-1);
+      done();
+    });
+}
+chai.use(chaiHttp);
+describe('fields模块', function(done) {
+  describe('POST /api/field', function(done) {
+    describe('ERROR', function(done) {
+      it('not tableId', function(done) {
+        chai
+          .request('http://localhost:8000')
+          .post('/api/field')
+          .send({
+            name: 'age',
+            fieldTypeId: '2',
+            typeOptions: {
+              format: 'decimal',
+              precision: 1,
+              negative: false,
+            },
+          })
+          .end((err, res) => {
+            res.should.have.status(422);
+            done();
+          });
+      });
+      it('error tableId', function(done) {
+        chai
+          .request('http://localhost:8000')
+          .post('/api/field')
+          .send({
+            tableId: '111111111',
+            name: 'age',
+            fieldTypeId: '2',
+            typeOptions: {
+              format: 'decimal',
+              precision: 1,
+              negative: false,
+            },
+          })
+          .end((err, res) => {
+            res.should.have.status(400);
+            done();
+          });
+      });
+      it('error fieldTypeId', function(done) {
+        chai
+          .request('http://localhost:8000')
+          .post('/api/field')
+          .send({
+            tableId: 'tblNGUPdSs9Va4X5u',
+            name: 'age',
+            fieldTypeId: '5',
+            typeOptions: {
+              format: 'decimal',
+              precision: 1,
+              negative: false,
+            },
+          })
+          .end((err, res) => {
+            res.should.have.status(400);
+            done();
+          });
+      });
+      it('error typeOptions', function(done) {
+        chai
+          .request('http://localhost:8000')
+          .post('/api/field')
+          .send({
+            tableId: 'tblNGUPdSs9Va4X5u',
+            name: 'age',
+            fieldTypeId: '2',
+            typeOptions: {
+              precision: 1,
+              negative: false,
+            },
+          })
+          .end((err, res) => {
+            res.should.have.status(422);
+            done();
+          });
+      });
+    });
+
+    describe('OK', function(done) {
+      it('text', function(done) {
+        chai
+          .request('http://localhost:8000')
+          .post('/api/field')
+          .send({
+            tableId: 'tblNGUPdSs9Va4X5u',
+            name: 'address',
+            fieldTypeId: '1',
+          })
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.have.property('fieldId');
+            checkNewField(res.body, done);
+          });
+      });
+      it('number', function(done) {
+        chai
+          .request('http://localhost:8000')
+          .post('/api/field')
+          .send({
+            tableId: 'tblNGUPdSs9Va4X5u',
+            name: 'age',
+            fieldTypeId: '2',
+            typeOptions: {
+              format: 'decimal',
+              precision: 1,
+              negative: false,
+            },
+          })
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.have.property('fieldId');
+            checkNewField(res.body, done);
+          });
+      });
+      it('foreignKey', function(done) {
+        chai
+          .request('http://localhost:8000')
+          .post('/api/field')
+          .send({
+            tableId: 'tblNGUPdSs9Va4X5u',
+            name: '测试',
+            fieldTypeId: '3',
+            typeOptions: {
+              relationship: 'many',
+              foreignTableId: 'tblsnmRLfttLmAYQ8',
+            },
+          })
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.have.property('foreignFieldId');
+            res.body.should.have.property('symmetricFieldId');
+            checkForeignField(res.body, done);
+          });
+      });
+      it('multipleAttachment', function(done) {
+        chai
+          .request('http://localhost:8000')
+          .post('/api/field')
+          .send({
+            tableId: 'tblNGUPdSs9Va4X5u',
+            name: 'contract',
+            fieldTypeId: '4',
+          })
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.have.property('fieldId');
+            checkNewField(res.body, done);
+          });
+      });
+    });
+  });
+});
