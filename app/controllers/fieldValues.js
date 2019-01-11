b743ebb089c25a166f9f8497bcb572e30405d688
Caminer-Carson <45706114+Caminer-Carson@users.noreply.github.com>
2019-01-11 10:23:07 +0800

    Put post delete field value (#45)

--- app/controllers/fieldValues.js
+++ app/controllers/fieldValues.js
@@ -17,9 +17,9 @@ const UPSERT_MAP = {
   number: upsertGenericFieldValue,
 };
 
-async function createMultipleAttachment({ fieldValueId, value }) {
+function createMultipleAttachment({ fieldValueId, value }) {
   checkKeyExists(value, 'url', 'fileName', 'fileType');
-  return await multipleAttachmentValues.create({
+  return multipleAttachmentValues.create({
     fieldValueId,
     ...value,
   });
@@ -32,7 +32,7 @@ function createForeignKeyValue({ fieldValueId, value }) {
     const symmetricFieldValue = await fieldValues
       .findCreateFind({ where: { recordId, fieldId } }, transact)
       .spread(fieldValue => fieldValue);
-    await foreignKeyValues.create(
+    return await foreignKeyValues.create(
       {
         fieldValueId,
         symmetricFieldValueId: symmetricFieldValue.id,
@@ -76,7 +76,7 @@ module.exports = {
     });
   },
 
-  createArrayType(params) {
+  createArrayValue(params) {
     const fieldProps = FIELD_TYPES[params.fieldTypeId];
     const createValue = CREATE_MAP[fieldProps.name];
     return createValue(params);
--- seeders/20181226084701-init-number-types.js
+++ seeders/20181226084701-init-number-types.js
@@ -11,6 +11,14 @@ module.exports = {
           createdAt: '2018-05-05T04:09:06.024Z',
           updatedAt: '2018-05-05T04:09:06.024Z',
         },
+        {
+          format: 'decimal',
+          precision: 1,
+          negative: false,
+          fieldId: 'fld1e1ce8af180400b',
+          createdAt: '2018-05-05T04:09:06.024Z',
+          updatedAt: '2018-05-05T04:09:06.024Z',
+        },
       ],
       {},
     );
