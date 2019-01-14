65d32ed018af00829f58aaccd7a92ab47eae47e0
Carson Cai <carson@caminer.io>
2019-01-14 13:01:00 +0800

    去除外建约束
    1、取消fields、records、foreignKeyTypes关于table的外建约束，转移到delete /api/table分支中

--- migrations/20181223093321-create-records.js
+++ migrations/20181223093321-create-records.js
@@ -1,35 +1,24 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface
-      .createTable('records', {
-        id: {
-          allowNull: false,
-          primaryKey: true,
-          type: Sequelize.STRING,
-        },
-        tableId: {
-          allowNull: false,
-          type: Sequelize.STRING,
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-      })
-      .then(() => {
-        return queryInterface.addConstraint('records', ['tableId'], {
-          type: 'FOREIGN KEY',
-          references: {
-            table: 'tables',
-            field: 'id',
-          },
-          onDelete: 'cascade',
-        });
-      });
+    return queryInterface.createTable('records', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      tableId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('records');
--- migrations/20181223093922-create-fields.js
+++ migrations/20181223093922-create-fields.js
@@ -1,43 +1,32 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface
-      .createTable('fields', {
-        id: {
-          allowNull: false,
-          primaryKey: true,
-          type: Sequelize.STRING,
-        },
-        name: {
-          allowNull: false,
-          type: Sequelize.STRING,
-        },
-        tableId: {
-          allowNull: false,
-          type: Sequelize.STRING,
-        },
-        fieldTypeId: {
-          allowNull: false,
-          type: Sequelize.INTEGER,
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-      })
-      .then(() => {
-        return queryInterface.addConstraint('fields', ['tableId'], {
-          type: 'FOREIGN KEY',
-          references: {
-            table: 'tables',
-            field: 'id',
-          },
-          onDelete: 'cascade',
-        });
-      });
+    return queryInterface.createTable('fields', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      tableId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      fieldTypeId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('fields');
