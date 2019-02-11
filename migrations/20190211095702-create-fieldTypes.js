9a2bce47702322759d513ed425730e3e4e19adef
Lim Wang <lim@caminer.io>
2019-02-11 18:19:45 +0800

    no message

---
+++ migrations/20190211095702-create-fieldTypes.js
@@ -0,0 +1,34 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('FieldTypes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+        indexes: [
+          {
+            unique: true,
+            fields: ['name'],
+          },
+        ],
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('FieldTypes');
+  },
+};
