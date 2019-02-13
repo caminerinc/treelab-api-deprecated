248cf1553888e500384ceccbd99c5dea33ea39a3
Lim Wang <lim@caminer.io>
2019-02-13 12:09:23 +0800

    seeds

--- seeders/20190212034222-fieldTypes.js
+++ seeders/20190212034222-fieldTypes.js
@@ -6,24 +6,28 @@ module.exports = {
         {
           id: '1',
           name: 'text',
+          isPrimitive: true,
           createdAt: '2018-12-24T04:09:06.024Z',
           updatedAt: '2018-12-24T04:09:06.024Z',
         },
         {
           id: '2',
           name: 'number',
+          isPrimitive: true,
           createdAt: '2018-12-24T04:09:06.024Z',
           updatedAt: '2018-12-24T04:09:06.024Z',
         },
         {
           id: '3',
           name: 'reference',
+          isArray: true,
           createdAt: '2018-12-24T04:09:06.024Z',
           updatedAt: '2018-12-24T04:09:06.024Z',
         },
         {
           id: '4',
           name: 'multipleAttachment',
+          isArray: true,
           createdAt: '2018-12-24T04:09:06.024Z',
           updatedAt: '2018-12-24T04:09:06.024Z',
         },
