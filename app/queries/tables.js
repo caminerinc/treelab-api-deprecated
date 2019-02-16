e1520e8da0ee97d8735c43b8a4989cb124d9d73c
Lim Wang <lim@caminer.io>
2019-02-16 17:08:43 +0800

    删除createdAt

--- app/queries/tables.js
+++ app/queries/tables.js
@@ -25,7 +25,7 @@ module.exports = {
       where: { id },
       include: [
         {
-          attributes: ['id', 'createdAt'],
+          attributes: ['id'],
           model: Records,
           as: 'recs',
           include: [
