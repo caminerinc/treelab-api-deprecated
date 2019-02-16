6210d1382281e1bbca43f48f8d15124122b23aa1
Lim Wang <lim@caminer.io>
2019-02-16 18:03:40 +0800

    no message

--- app/queries/tables.js
+++ app/queries/tables.js
@@ -36,26 +36,26 @@ module.exports = {
             },
           ],
         },
-        {
-          model: FieldPositions,
-          as: 'fieldPositions',
-          attributes: ['siblingId'],
-          // include: [
-          //   {
-          //     model: Fields,
-          //     as: 'field',
-          //     attributes: ['width'],
-          //   },
-          // ],
-        },
         // {
-        //   model: RecordPositions,
-        //   as: 'recordPositions',
+        //   model: FieldPositions,
+        //   as: 'fieldPositions',
         //   attributes: ['siblingId'],
+        //   // include: [
+        //   //   {
+        //   //     model: Fields,
+        //   //     as: 'field',
+        //   //     attributes: ['width'],
+        //   //   },
+        //   // ],
         // },
+        {
+          model: RecordPositions,
+          as: 'recordPositions',
+          attributes: ['siblingId'],
+        },
       ],
       order: [
-        [sequelize.col('fieldPositions.position'), 'asc'],
+        // [sequelize.col('fieldPositions.position'), 'asc'],
         // [sequelize.col('recordPositions.position'), 'asc'],
       ],
     });
