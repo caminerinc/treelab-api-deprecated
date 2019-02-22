53610360eb945cad25560fb380fe8f2a57af2dda
Lim Wang <lim@caminer.io>
2019-02-22 17:28:50 +0800

    no message

--- app/resolvers/apps.js
+++ app/resolvers/apps.js
@@ -3,7 +3,6 @@ const appsController = require('../controllers/apps');
 
 module.exports = {
   async getAll(ctx) {
-    const result = await appsController.getAll();
-    ctx.body = result.map(i => pick(i, ['id', 'name', 'description']));
+    ctx.body = await appsController.getAll();
   },
 };
