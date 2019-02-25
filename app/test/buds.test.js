4c7c02ee7362fcea38b49d8c739ae3cb51ed1fb9
limwcj <45704878+limwcj@users.noreply.github.com>
2019-02-25 13:26:36 +0800

    getBud (#113)

--- app/test/buds.test.js
+++ app/test/buds.test.js
@@ -5,6 +5,7 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('buds模块', function() {
+  let budId = '';
   describe('create', function() {
     it('ok', function(done) {
       chai
@@ -13,6 +14,29 @@ describe('buds模块', function() {
         .send({ appId: 1 })
         .end((err, res) => {
           res.should.have.status(200);
+          budId = res.body.budId;
+          done();
+        });
+    });
+  });
+  describe('getBud', function() {
+    it('ok', function(done) {
+      chai
+        .request('http://localhost:8000')
+        .get(`/api/bud/${budId}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('id');
+          res.body.should.have.property('name');
+          done();
+        });
+    });
+    it('err_baseId_missing', function(done) {
+      chai
+        .request('http://localhost:8000')
+        .get('/api/bud/')
+        .end((err, res) => {
+          res.should.have.status(405);
           done();
         });
     });
